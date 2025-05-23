d264ebfde493feb9e31d4f04b86867e762c82410
masterboss5 <robloxguru1114@gmail.com>
2025-05-23 01:07:13 -0400

    Added keyboard class

---
+++ src/io/Keyboard.java
@@ -0,0 +1,20 @@
+package io;
+
+public class Keyboard implements IInputHandler {
+
+
+    @Override
+    public void createCallbacks() {
+
+    }
+
+    @Override
+    public void bindCallbacks() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+}
