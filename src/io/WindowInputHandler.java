d2194fbb95670c4491d4d8c271ba32b4f1e8fca8
masterboss5 <robloxguru1114@gmail.com>
2025-05-29 01:40:48 -0400

    added window input handling

---
+++ src/io/WindowInputHandler.java
@@ -0,0 +1,30 @@
+package io;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWWindowSizeCallback;
+
+public class WindowInputHandler implements IInputHandler {
+    private Window window;
+    private long windowAddress;
+    private GLFWWindowSizeCallback windowResizeCallback;
+
+    @Override
+    public void createCallbacks() {
+        this.windowResizeCallback = new GLFWWindowSizeCallback() {
+            @Override
+            public void invoke(long window, int width, int height) {
+
+            }
+        };
+    }
+
+    @Override
+    public void bindCallbacks() {
+        GLFW.glfwSetWindowSizeCallback(this.windowAddress, this.windowResizeCallback);
+    }
+
+    @Override
+    public void cleanup() {
+        this.windowResizeCallback.free();
+    }
+}
