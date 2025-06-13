16114a2044343cda7b2180e10b07865529175f29
masterboss5 <robloxguru1114@gmail.com>
2025-06-12 23:49:41 -0400

    more uniform methods to glUniformManager

--- src/shader/glUniformManager.java
+++ src/shader/glUniformManager.java
@@ -1,8 +1,14 @@
 package shader;
 
 import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL46;
 import util.Math;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
 
 public interface glUniformManager {
     int getProgramID();
@@ -41,6 +47,50 @@ public interface glUniformManager {
 
     default void setUniform4i(String location, int i1, int i2, int i3, int i4) {
         GL46.glUniform4i(getLocation(location), i1, i2, i3, i4);
+    }
+
+    default void setUniform1fv(String location, FloatBuffer f1) {
+        glUniform1fv(getLocation(location), f1);
+    }
+
+    default void setUniform2fv(String location, FloatBuffer f1) {
+        glUniform2fv(getLocation(location), f1);
+    }
+
+    default void setUniform3fv(String location, FloatBuffer f1) {
+        glUniform3fv(getLocation(location), f1);
+    }
+
+    default void setUniform4fv(String location, FloatBuffer f1) {
+        glUniform4fv(getLocation(location), f1);
+    }
+
+    default void setUniform1iv(String location, IntBuffer i1) {
+        GL20.glUniform1iv(getLocation(location), i1);
+    }
+
+    default void setUniform2iv(String location, IntBuffer i1) {
+        GL20.glUniform2iv(getLocation(location), i1);
+    }
+
+    default void setUniform3iv(String location, IntBuffer i1) {
+        GL20.glUniform3iv(getLocation(location), i1);
+    }
+
+    default void setUniform4iv(String location, IntBuffer i1) {
+        GL20.glUniform4iv(getLocation(location), i1);
+    }
+
+    default void setUniformMatrix2fv(String location, boolean transpose, FloatBuffer f1) {
+        glUniformMatrix2fv(getLocation(location), transpose, f1);
+    }
+
+    default void setUniformMatrix3fv(String location, boolean transpose, FloatBuffer f1) {
+        glUniformMatrix3fv(getLocation(location), transpose, f1);
+    }
+
+    default void setUniformMatrix4fv(String location, boolean transpose, FloatBuffer f1) {
+        glUniformMatrix4fv(getLocation(location), transpose, f1);
     }
 
     default void setUniformMatrix4f(String location, Matrix4f m1) {
