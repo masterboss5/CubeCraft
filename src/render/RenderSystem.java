a17b3093b1861c4428c190493a8805d3f9e8a442
masterboss5 <robloxguru1114@gmail.com>
2025-06-19 01:32:44 -0400

    debugging

--- src/render/RenderSystem.java
+++ src/render/RenderSystem.java
@@ -80,8 +80,6 @@ public class RenderSystem {
         GL13.glBindTexture(GL13.GL_TEXTURE_2D, model.getTextureID());
         GL46.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, model.getVertexBuffer().getIndexBufferObject().getID());
 
-        System.out.println(Arrays.toString(model.getVertexBuffer().getIndices()));
-
         GL46.glDrawElements(GL46.GL_TRIANGLES, model.getIndicesCount(), GL46.GL_UNSIGNED_INT, 0);
 //        GL46.glDrawElements(GL46.GL_TRIANGLES, model.getIndicesCount(), GL46.GL_UNSIGNED_INT, 0);
 
