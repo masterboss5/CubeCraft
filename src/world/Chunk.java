6bfe1afd91171eb40622cd902e043032eef26c81
masterboss5 <robloxguru1114@gmail.com>
2025-06-21 03:22:17 -0400

    added chunks

---
+++ src/world/Chunk.java
@@ -0,0 +1,7 @@
+package world;
+
+import block.Block;
+
+public class Chunk {
+    Block[][][] blockGrid = new Block[16][256][16];
+}
---
+++ src/world/ChunkReigon.java
@@ -0,0 +1,5 @@
+package world;
+
+public class ChunkReigon {
+    Chunk[][] chunks = new Chunk[3][3];
+}
