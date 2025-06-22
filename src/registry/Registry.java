da54433d2193f4389bf650b6736b998874eae1eb
masterboss5 <robloxguru1114@gmail.com>
2025-06-22 15:28:21 -0400

    registry

--- src/registry/Registry.java
+++ src/registry/Registry.java
@@ -6,7 +6,11 @@ import java.util.Set;
 public interface Registry<ENTRY> {
     ENTRY get(ResourceLocation key);
 
+    ENTRY get(RegistryKey<ENTRY> registryKey);
+
     ENTRY register(ResourceLocation key, ENTRY entry);
+
+    ENTRY register(RegistryKey<ENTRY> registryKey, ENTRY entry);
 
     Collection<ENTRY> values();
 
--- src/registry/SimpleRegistry.java
+++ src/registry/SimpleRegistry.java
@@ -11,8 +11,20 @@ public class SimpleRegistry<ENTRY> implements Registry<ENTRY> {
     }
 
     @Override
+    public ENTRY get(RegistryKey<ENTRY> registryKey) {
+        return this.get(registryKey.getKey());
+    }
+
+    @Override
     public ENTRY register(ResourceLocation key, ENTRY entry) {
         this.entries.put(key, entry);
+        return entry;
+    }
+
+    @Override
+    public ENTRY register(RegistryKey<ENTRY> registryKey, ENTRY entry) {
+        this.register(registryKey.getKey(), entry);
+
         return entry;
     }
 
