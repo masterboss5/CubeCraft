5d6ae084faf754812eb7c1ed4a0e87a5dbe15aad
masterboss5 <robloxguru1114@gmail.com>
2025-06-28 05:03:47 -0400

    chunk system

--- src/world/Chunk.java
+++ src/world/Chunk.java
@@ -3,7 +3,7 @@ package world;
 import block.Block;
 
 public class Chunk {
-    Block[][][] blockGrid = new Block[16][256][16];
+    Block[][][] blockGrid = new Block[ChunkPosition.CHUNK_WIDTH][ChunkPosition.CHUNK_HEIGHT][ChunkPosition.CHUNK_WIDTH];
 
     protected Chunk() {
 
