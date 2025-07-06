51cbf224352542729b2bd44b5eafc7433750f745
masterboss5 <robloxguru1114@gmail.com>
2025-07-06 04:40:04 -0400

    refactoring

--- src/shader/TexturedShaderProgram.java
+++ src/shader/TexturedShaderProgram.java
@@ -1,7 +1,5 @@
 package shader;
 
-import graphic.Texture;
-
 public class TexturedShaderProgram extends ShaderProgram {
     TexturedShaderProgram(String vertexShaderFilePath, String fragmentShaderFilePath) {
         super(vertexShaderFilePath, fragmentShaderFilePath);
