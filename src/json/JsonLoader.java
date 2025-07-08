ed6edb13704ea694815acaec5e2da4d1d3fc3d22
masterboss5 <robloxguru1114@gmail.com>
2025-07-08 14:18:03 -0400

    refactoring

--- src/json/JsonLoader.java
+++ src/json/JsonLoader.java
@@ -2,6 +2,8 @@ package json;
 
 import com.google.gson.Gson;
 import exception.JsonParsingException;
+import gl.VertexBuffer;
+import gl.glUsage;
 import graphic.BlockModel;
 import graphic.ModelBuilder;
 import texture.Texture;
@@ -31,19 +33,21 @@ public class JsonLoader {
                 new Texture(ROOT_TEXTURE_PATH + readBlockModel.texture()));
     }
 
-    public static BlockModel readSimpleBlockModelJson(String fileName) {
-        ReadTexturedBlockModel readBlockModel;
-
-        try (FileReader fileReader = new FileReader(ROOT_MODEL_PATH + fileName)) {
-            readBlockModel = INSTANCE.fromJson(fileReader, ReadTexturedBlockModel.class);
-        } catch (IOException exception) {
-            throw new JsonParsingException(JsonParsingException.LoadingContext.MODEL, fileName);
-        }
-
-        return ModelBuilder.buildBlockModel(Registries.SHADER_PROGRAM.get(readBlockModel.shader_program()),
-                readBlockModel.vertices(),
-                readBlockModel.indices(),
-                readBlockModel.uv(),
-                new Texture(ROOT_TEXTURE_PATH + readBlockModel.texture()));
-    }
+//    public static BlockModel readSimpleBlockModelJson(String fileName) {
+//        ReadTexturedBlockModel readBlockModel;
+//
+//        try (FileReader fileReader = new FileReader(ROOT_MODEL_PATH + fileName)) {
+//            readBlockModel = INSTANCE.fromJson(fileReader, ReadTexturedBlockModel.class);
+//        } catch (IOException exception) {
+//            throw new JsonParsingException(JsonParsingException.LoadingContext.MODEL, fileName);
+//        }
+//
+//        VertexBuffer vertexBuffer = new VertexBuffer(glUsage.GL_STATIC_DRAW)
+//                .vertexes(new float[] {1})
+//                .indices(new int[] {1});
+//
+//
+//
+//        vertexBuffer.build();
+//    }
 }
