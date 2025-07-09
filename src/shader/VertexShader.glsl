9b76f834f7f0716b1a6e38ac8fa4b8f3342e397d
masterboss5 <robloxguru1114@gmail.com>
2025-07-09 04:50:40 -0400

    finished adding new texture system

--- src/shader/VertexShader.glsl
+++ src/shader/VertexShader.glsl
@@ -1,10 +1,8 @@
 #version 400 core
 
-in vec3 position;
+layout(location = 0) in vec3 position;
 
 out vec3 color;
-
-uniform vec3 colorValue;
 
 uniform mat4 transform;
 uniform mat4 projection;
@@ -13,6 +11,6 @@ uniform mat4 view;
 void main(void)
 {
     gl_Position = projection * view * transform * vec4(position, 1);
-    color = vec3((position.x + 1)/50, (position.y + 0.5)/50, (position.z + 0)/50);
+    color = vec3((position.x + 1) / 50, (position.y + 0.5)/50, (position.z + 0)/50);
 //    color = vec3(colorValue);
 }
